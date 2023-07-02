5b304907e8ce6a0ec12d4489bf7376acc2239d65
krisszcode <46415982+krisszcode@users.noreply.github.com>
2023-07-02 17:03:54 +0200

    init

---
+++ frontend/next.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "/src/app/components"),
+      "@hooks": path.resolve(__dirname, "/src/app/hooks"),
+      "@styles": path.resolve(__dirname, "/src/app/styles"),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
