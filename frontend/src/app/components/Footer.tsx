ec9ac2e12b4e1c8ba8f1b495b249c2cca98c0490
krisszcode <46415982+krisszcode@users.noreply.github.com>
2023-07-06 17:17:46 +0200

    layout work in progress

---
+++ frontend/src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                lába
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                feje
+            </div>
+        );
+    }
+}
+
+export default Header;
