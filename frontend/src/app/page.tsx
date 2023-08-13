fcdf1fc9a0909b07c7d862ce31f7fa8ee41884ea
krisszcode <46415982+krisszcode@users.noreply.github.com>
2023-08-13 21:26:08 +0200

    ADD: heroimage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ function Home() {
     <Layout>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-          <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+          <img src="/heroImage.jpg" className="max-w-sm rounded shadow-2xl" />
           <div>
             <h1 className="text-5xl font-bold">Krisztián Nagy | Full Stack Web Developer</h1>
             <p className="py-6">From the bustling streets of Vienna to the ever-evolving corridors of the web, I channel my passion for development into every project. My mission? To craft intuitive, meaningful experiences. With a multilingual palette and a knack for bridging ideas, I believe the best digital stories are yet to be told. Let's collaborate and write the next one.</p>
