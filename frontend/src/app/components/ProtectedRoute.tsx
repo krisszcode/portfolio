3e4d54fc40d07d3ec076273c304a8fc5b72cc664
krisszcode <46415982+krisszcode@users.noreply.github.com>
2023-10-21 11:03:08 +0200

    update next, and add protected routes

--- frontend/src/app/components/ProtectedRoute.tsx
+++ frontend/src/app/components/ProtectedRoute.tsx
@@ -1,27 +1,16 @@
-'use client'
-import { ReactNode, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { useAuth } from '../context/authContext';
 
-interface ProtectedRouteProps {
-    children: ReactNode;
-  }
-
-export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const isAuthenticated = useAuth(); // Feltételezzük, hogy van egy ilyen hook
-
-  const router = useRouter()
+export const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
 
   useEffect(() => {
     if (!isAuthenticated) {
       router.push('/login');
     }
   }, [isAuthenticated]);
-  
-
-  if (typeof window === 'undefined') {
-    return null; // SSR alatt ne rendereljünk semmit
-  }
 
   return (
     <>
