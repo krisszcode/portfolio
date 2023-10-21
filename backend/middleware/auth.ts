91d8a53f9d516867a0e2cbe02622b9cbe18b6bd3
krisszcode <46415982+krisszcode@users.noreply.github.com>
2023-10-21 11:04:30 +0200

    fix

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -4,18 +4,17 @@ import jwt from 'jsonwebtoken';
 
 // Middleware az authentikációhoz
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['authorization']; // Token az Authorization headerből
+  const token = req.headers['authorization']; 
 
   if (!token) {
     return res.status(401).send('Unauthorized');
   }
 
-  // Token ellenőrzése
   jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
     if (err) {
       return res.status(401).send('Invalid token');
     }
-    req.user = decoded; // Mentjük a dekódolt adatokat a requestbe
-    next(); // Tovább a következő middleware-re vagy route-ra
+    req.user = decoded; 
+    next(); 
   });
 };
