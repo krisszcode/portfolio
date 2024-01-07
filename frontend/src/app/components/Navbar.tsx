cd7634d68394277c54155cfb4d1e218a5e03c617
krisszcode <46415982+krisszcode@users.noreply.github.com>
2024-01-07 18:02:47 +0100

    Add links to Navbar component

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
 import Logo from "./Logo";
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
@@ -9,15 +10,21 @@ const Navbar = () => {
       <div className="flex text-xl font-bold items-center">
         <ThemeSwitcher />
         <div className="flex space-x-8 mr-8">
-          <button className="relative hover:after:absolute after:bg-slate-700 dark:after:bg-white after:blur after:z-0 after:inset-0 after:bottom-2 after:top-3">
+          <Link href="/">
+          <div className="relative hover:after:absolute after:bg-slate-700 dark:after:bg-white after:blur after:z-0 after:inset-0 after:bottom-2 after:top-3">
             <p className="z-10">Home</p>
-          </button>
-          <button className="relative hover:after:absolute after:bg-slate-700 dark:after:bg-white after:blur after:z-0 after:inset-0 after:bottom-2 after:top-3">
+          </div>
+          </Link>
+          <Link href="/projects">
+          <div className="relative hover:after:absolute after:bg-slate-700 dark:after:bg-white after:blur after:z-0 after:inset-0 after:bottom-2 after:top-3">
             <p className="z-10  sm:block hidden">Projects</p>
-          </button>
-          <button className="relative hover:after:absolute after:bg-slate-700 dark:after:bg-white after:blur after:z-0 after:inset-0 after:bottom-2 after:top-3">
+          </div>
+          </Link>
+          <Link href="/contacts">
+          <div className="relative hover:after:absolute after:bg-slate-700 dark:after:bg-white after:blur after:z-0 after:inset-0 after:bottom-2 after:top-3">
             <p className="z-10 sm:block hidden" >Contacts</p>
-          </button>
+          </div>
+          </Link>
         </div>
       </div>
     </nav>
