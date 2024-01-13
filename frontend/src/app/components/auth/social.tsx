1468e848719d4e6bd91c2cb449499756c1cc9f86
krisszcode <46415982+krisszcode@users.noreply.github.com>
2024-01-13 20:53:04 +0100

    Refactor social login buttons

--- frontend/src/app/components/auth/social.tsx
+++ frontend/src/app/components/auth/social.tsx
@@ -2,14 +2,12 @@ import { FaGithub, FaFacebook } from 'react-icons/fa';
 
 const Social = () => {
     return (
-        <div>
-            <button>
-                <FaGithub />
-                Login with GitHub
+        <div className='flex items-center justify-center w-full gap-x-2 mt-4'>
+            <button onClick={()=>{}} className='outline-lg bg-gray-200 p-2 rounded-full'>
+                <FaGithub className='text-4xl' />
             </button>
-            <button>
-                <FaFacebook />
-                Login with Facebook
+            <button onClick={()=>{}} className='outline-lg bg-blue-500 p-2 rounded-full'>
+                <FaFacebook className='text-4xl text-white' />
             </button>
         </div>
     );
