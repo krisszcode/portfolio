a5570650d7edcdc3e2016b7e69dc12277ea5630c
krisszcode <46415982+krisszcode@users.noreply.github.com>
2024-01-14 12:48:27 +0100

    Update styles in social and form-success components

--- frontend/src/app/components/auth/social.tsx
+++ frontend/src/app/components/auth/social.tsx
@@ -4,7 +4,7 @@ import { FcGoogle  } from 'react-icons/fc';
 const Social = () => {
     return (
         <div className='flex items-center justify-center w-full gap-x-2'>
-            <button onClick={()=>{}} className='outline-lg p-2 rounded-full bg-gray-100'>
+            <button onClick={()=>{}} className='outline-lg p-2 rounded-full bg-gray-100 dark:bg-black'>
                 <FcGoogle className='text-3xl text-white' />
             </button>
             <button onClick={()=>{}} className='outline-lg bg-gray-100 dark:bg-black p-2 rounded-full'>
--- frontend/src/app/components/form-success.tsx
+++ frontend/src/app/components/form-success.tsx
@@ -8,7 +8,7 @@ export const FormSuccess = ({ message }: FormSuccessProps) => {
     if (!message) return null;
 
     return (
-        <div className="bg-emerald-500 flex items-center space-x-2">
+        <div className="bg-emerald-100 text-emerald-500 flex items-center text-sm rounded-md gap-x-2 p-3">
             <CheckCircledIcon className="h-4 w-4" />
             <span className="">{message}</span>
         </div>
